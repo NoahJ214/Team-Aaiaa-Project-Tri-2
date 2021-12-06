d859e5d72c3620e837575e842a2478da761ce362
Ritvik-Keerthi <89219486+Ritvik-Keerthi@users.noreply.github.com>
2021-12-06 12:21:54 -0800

    reset everything except README to create layout and navbar properly

--- main.py
+++ main.py
@@ -6,46 +6,31 @@ app = Flask(__name__)
 
 
 # connects default URL to render index.html
-
-@app.route('/william/')
-def greet6():
-    return render_template("william.html")
-
-@app.route('/william/', methods=['GET', 'POST'])
-def greet7():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("william.html", name1=name)
-    # starting and empty input default
-    return render_template("william.html", name1="World")
-
-@app.route('//')
+@app.route('/')
 def index():
     return render_template("index.html")
 
 
 # connects /kangaroos path to render kangaroos.html
-@app.route("/ritvik/")
-def ritvik():
-    return render_template("ritvik.html")
+@app.route('/kangaroos/')
+def kangaroos():
+    return render_template("kangaroos.html")
 
-@app.route("/william/")
-def william():
-    return render_template("william.html")
 
-@app.route("/timothy/")
-def timothy():
-    return render_template("timothy.html")
+@app.route('/walruses/')
+def walruses():
+    return render_template("walruses.html")
 
-@app.route("/nathan/")
-def nathan():
-    return render_template("nathan.html")
 
-@app.route("/noah")
-def noah():
-    return render_template("noah.html")
+@app.route('/hawkers/')
+def hawkers():
+    return render_template("hawkers.html")
+
+
+@app.route('/stub/')
+def stub():
+    return render_template("stub.html")
+
 
 # runs the application on the development server
 if __name__ == "__main__":
