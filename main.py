c0c9e21fa2a58a8b87a2d9d9f6861eafdfc76e46
Ritvik-Keerthi <89219486+Ritvik-Keerthi@users.noreply.github.com>
2021-12-13 12:38:43 -0800

    search functionality working, need to work on css to format it

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from api.nfl import nfl_nfl
 from api.tennis import app_tennis
 from api.soccer import app_soccer
 from api.baseball import app_baseball
+from crud.app_crud import app_crud
 from flask import render_template, request, Flask
 from flask import Flask
+
 # create a Flask instance
 app = Flask(__name__)
 app.register_blueprint(app_api)
@@ -15,6 +17,7 @@ app.register_blueprint(nfl_nfl)
 app.register_blueprint(app_tennis)
 app.register_blueprint(app_soccer)
 app.register_blueprint(app_baseball)
+app.register_blueprint(app_crud)
 
 # connects default URL to render index.html
 @app.route('/')
